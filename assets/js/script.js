557da3a161f7d285f4456254a3fd065dc9658fa8
EdytaJ <edyta_jakubek@yahoo.com>
2022-06-15 23:10:36 +0000

    Work on Javascript code for navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 const headerScroll = () => {
     const headerElement = document.querySelector('.header');
     this.scrollY >= 50 ? headerElement.classList.add('active') : headerElement.classList.remove('active');
-}
+};
 window.addEventListener('scroll', headerScroll);
 
 /* Open and close manu on on icon click*/
 const menuToggler = document.querySelector('#menu-toggler');
-const navbarMenu = document.querySelector('navbar__menu');
+const navbarMenu = document.querySelector('.navbar__menu');
 const toggleMenu = () => {
     navbarMenu.classList.toggle('active');
-}
+};
 menuToggler.addEventListener('click', toggleMenu);
 
 const linksToggleMenu = (e) => {
-    if(e.target.classList.contains('navbar__list-link')) navbarMenu.classList.remove('active');
-}
+    if(e.target.classList.contains('.navbar__list-link')) navbarMenu.classList.remove('active');
+};
+window.addEventListener('click', linksToggleMenu);
