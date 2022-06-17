d8e66815beabf5975b8d66fa9f17dcb57282038e
EdytaJ <edyta_jakubek@yahoo.com>
2022-06-17 22:46:50 +0000

    Add about me pictures and javascript swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,22 @@ const linksToggleMenu = (e) => {
     if(e.target.classList.contains('.navbar__list-link')) navbarMenu.classList.remove('active');
 };
 window.addEventListener('click', linksToggleMenu);
+
+const swiper = new Swiper('.myswiper', {
+    effect: 'coverflow', 
+    grabCursor: true,
+    centeredSlides: true,
+    slidePerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,  
+    },
+
+    pagination: {
+        el: '.swiper-pagination'
+    }
+}
+)
