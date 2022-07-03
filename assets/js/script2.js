d8b3d57813da3777f5a1e49ccd122748bd55478b
EdytaJ <edyta_jakubek@yahoo.com>
2022-07-03 20:49:59 +0000

    Add style to thank you card pop ups

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -6,14 +6,14 @@ const greeting = document.querySelector(".personal-greeting");
 function nameDisplayCheck() {
   if (localStorage.getItem("name")) {
     let name = localStorage.getItem("name");
-    h1.textContent = `Welcome ${name}!`;
-    greeting.textContent = `Welcome to your yoga class, ${name}! I hope you will love it!`;
+    h1.textContent = `Thank you ${name}!`;
+    greeting.textContent = `See you in Savasana ${name}! I hope you will love it!`;
 
     forgetMe.style.display = "block";
     rememberMe.style.display = "none";
   } else {
     h1.textContent = `Maybe next time..`;
-    greeting.textContent = `Check out my social media for more inspiration.`;
+    greeting.textContent = `Need more inspiration? Check out my social media  or get in touch if you have any questions`;
 
     forgetMe.style.display = "none";
     rememberMe.style.display = "block";
