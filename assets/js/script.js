9d5628629848f3bffadf87ee54ebc07b42d9ec28
EdytaJ <edyta_jakubek@yahoo.com>
2022-08-17 16:45:03 +0000

    Clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,133 +2,97 @@
 /* REVEAL EACH SECTION WHEN SCROLLING*/
 
 const allSections = document.querySelectorAll('.section');
-
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.remove('section__hidden');
-  observer.unobserve(entry.target);
+const revealSection = function(entries, observer) {
+	const [entry] = entries;
+	if (!entry.isIntersecting) return;
+	entry.target.classList.remove('section__hidden');
+	observer.unobserve(entry.target);
 };
-
 const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
+	root: null,
+	threshold: 0.15,
 });
-
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add('section__hidden');
+allSections.forEach(function(section) {
+	sectionObserver.observe(section);
+	section.classList.add('section__hidden');
 });
-
 /* Open and close manu on an icon click*/
 const menuToggler = document.querySelector('#menu-toggler');
 const navbarMenu = document.querySelector('.navbar__menu');
-
 const toggleMenu = () => {
-    navbarMenu.classList.toggle('active');
+	navbarMenu.classList.toggle('active');
 };
 menuToggler.addEventListener('click', toggleMenu);
-
-
 /*SWIPER*/
-
 const swiper = new Swiper('.myswiper', {
-    effect: 'coverflow', 
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: '2',
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,  
-    },
-
-    pagination: {
-        el: '.swiper-pagination'
-    }
-}
-);
-
+	effect: 'coverflow',
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: '2',
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows: true,
+	},
+	pagination: {
+		el: '.swiper-pagination'
+	}
+});
 /* MODAL POP UP*/
-
 const infoWindow = document.querySelector('.popup');
 const overlay = document.querySelector('.overlay');
 const btnClosePopup = document.querySelector('.close_popup');
 const btnsOpenPopup = document.querySelectorAll('.show_popup');
-
-const openPopup = function () {
-  infoWindow.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openPopup = function() {
+	infoWindow.classList.remove('hidden');
+	overlay.classList.remove('hidden');
 };
-
-const closePopup = function () {
-  infoWindow.classList.add('hidden');
-  overlay.classList.add('hidden');
+const closePopup = function() {
+	infoWindow.classList.add('hidden');
+	overlay.classList.add('hidden');
 };
-
-for (let i = 0; i < btnsOpenPopup.length; i++)
-  btnsOpenPopup[i].addEventListener('click', openPopup);
-
+for (let i = 0; i < btnsOpenPopup.length; i++) btnsOpenPopup[i].addEventListener('click', openPopup);
 btnClosePopup.addEventListener('click', closePopup);
 overlay.addEventListener('click', closePopup);
-
-document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
-
-  if (e.key === 'Escape' && !infoWindow.classList.contains('hidden')) {
-    closePopup();
-  }
+document.addEventListener('keydown', function(e) {
+	// console.log(e.key);
+	if (e.key === 'Escape' && !infoWindow.classList.contains('hidden')) {
+		closePopup();
+	}
 });
-
 /* Open and Close submission form*/
-
 function openForm() {
-    document.getElementById("signupForm").style.display = "block";
-  }
-  function closeForm() {
-    document.getElementById("signupForm").style.display = "none";
-  }
+	document.getElementById("signupForm").style.display = "block";
+}
 
+function closeForm() {
+	document.getElementById("signupForm").style.display = "none";
+}
 /* Save name and date from form*/
-
-  const form = document.querySelector("form");
-  const getName = document.querySelector("#entername");
-  const submitBtn = document.querySelector("#submitname");
-  const getDate = document.querySelector("#classdate");
-  
-
-  
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-  });
-  
-  submitBtn.addEventListener("click", function () {
-    localStorage.setItem("name", getName.value);
-    localStorage.setItem("date", getDate.value);
-   
-  });
-
-  /* Required fields on the form*/
-  
-
-  function navigate() {
-    let entername = document.getElementById('entername').value;
-    let email = document.getElementById('email').value;
-   let classdate = document.getElementById('classdate').value;
-   let classname = document.getElementById('classname').value;
- 
-    if (entername == "" || email == "" || classdate =="" || classname =="") {
-      alert("Please fill in all fields");
-     
-    } else {
-      window.open("thankyou.html", '_parent');
-    }
-  }
-
- 
+const form = document.querySelector("form");
+const getName = document.querySelector("#entername");
+const submitBtn = document.querySelector("#submitname");
+const getDate = document.querySelector("#classdate");
+form.addEventListener("submit", function(e) {
+	e.preventDefault();
+});
+submitBtn.addEventListener("click", function() {
+	localStorage.setItem("name", getName.value);
+	localStorage.setItem("date", getDate.value);
+});
+/* Required fields on the form*/
+function navigate() {
+	let entername = document.getElementById('entername').value;
+	let email = document.getElementById('email').value;
+	let classdate = document.getElementById('classdate').value;
+	let classname = document.getElementById('classname').value;
+	if (entername == "" || email == "" || classdate == "" || classname == "") {
+		alert("Please fill in all fields");
+	} else {
+		window.open("thankyou.html", '_parent');
+	}
+}
 
 
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,19 +1,15 @@
 const h1 = document.querySelector("h1");
 const greeting = document.querySelector(".personal-greeting");
 
-
 function nameDisplayCheck() {
-  if (localStorage.getItem("name", "date")) {
-    let name = localStorage.getItem("name");
-    let date = localStorage.getItem("date");
-    h1.textContent = `Thank you ${name}!`;
-    greeting.textContent = `See you in Savasana studio on ${date}`;
-  } else {
-    h1.textContent = `Maybe next time..`;
-    greeting.textContent = `Need more inspiration? Check out my social media links below or get in touch if you have any questions`;
-
-  }
+	if (localStorage.getItem("name", "date")) {
+		let name = localStorage.getItem("name");
+		let date = localStorage.getItem("date");
+		h1.textContent = `Thank you ${name}!`;
+		greeting.textContent = `See you in Savasana studio on ${date}`;
+	} else {
+		h1.textContent = `Maybe next time..`;
+		greeting.textContent = `Need more inspiration? Check out my social media links below or get in touch if you have any questions`;
+	}
 }
 document.body.onload = nameDisplayCheck;
-
-
